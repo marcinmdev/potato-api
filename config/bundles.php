6b3390069fd37dfe6679c5fdabc227a0fbfc3bc6
marcinmdev <marcin@insanet.pl>
2022-07-14 19:16:03 +0200

    swagger #1

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
--- src/Controller/V1/IngredientController.php
+++ src/Controller/V1/IngredientController.php
@@ -2,13 +2,25 @@
 
 namespace App\Controller\V1;
 
+use App\Entity\Ingredient;
 use App\Repository\IngredientRepository;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/ingredient')]
+#[OA\Response(
+    response: 200,
+    description: 'Returns the rewards of an user',
+    content: new OA\JsonContent(
+        type: 'array',
+        items: new OA\Items(ref: new Model(type: Ingredient::class, groups: ['index']))
+    )
+)]
+#[OA\Tag(name: 'ingredients')]
 class IngredientController extends AbstractController
 {
     #[Route('/', name: 'app_ingredient_index', methods: ['GET'])]
