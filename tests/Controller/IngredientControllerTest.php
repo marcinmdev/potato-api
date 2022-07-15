3be5a030f6236c9b8b207345f3e8df4091ea3f07
marcinmdev <marcin@insanet.pl>
2022-07-15 21:01:04 +0200

    ingredient CRUD test

--- tests/Controller/IngredientControllerTest.php
+++ tests/Controller/IngredientControllerTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Controller;
 use App\Story\UserAccountStory;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Throwable;
 
 /**
  * @covers \App\Controller\V1\IngredientController
@@ -19,6 +20,52 @@ class IngredientControllerTest extends WebTestCase
         $client = $this->createAuthenticatedClient();
 
         $client->request('GET', '/v1/ingredient/');
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * @covers \App\Controller\V1\IngredientController::get()
+     */
+    public function testGet(): void
+    {
+        $client = $this->createAuthenticatedClient();
+
+        $client->request('GET', '/v1/ingredient/1');
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * @covers \App\Controller\V1\IngredientController::create()
+     */
+    public function testCreate(): void
+    {
+        $client = $this->createAuthenticatedClient();
+
+        try {
+            $client->request(
+                method: 'POST',
+                uri: '/v1/ingredient/',
+                content: json_encode([
+                    'name' => 'Big Potato',
+                    'weight' => 10,
+                    'price' => 20,
+                ], JSON_THROW_ON_ERROR)
+            );
+        } catch (Throwable $e) {
+            $this->fail($e->getMessage());
+        }
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * @covers \App\Controller\V1\IngredientController::delete()
+     */
+    public function testDelete(): void
+    {
+        $client = $this->createAuthenticatedClient();
+
+        $client->request('DELETE', '/v1/ingredient/1');
         $this->assertResponseIsSuccessful();
     }
 
